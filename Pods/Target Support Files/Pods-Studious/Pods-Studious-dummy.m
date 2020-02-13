a28650bf3661bf3637f10ee901e8678a98779f11
Gregory B Driscoll <gbdrisco@lcs-209-165.syr.edu>
2020-02-12 19:20:26 -0500

    testing

---
+++ Pods/Target Support Files/Pods-Studious/Pods-Studious-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Studious : NSObject
+@end
+@implementation PodsDummy_Pods_Studious
+@end
---
+++ Pods/Target Support Files/Pods-StudiousTests/Pods-StudiousTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StudiousTests : NSObject
+@end
+@implementation PodsDummy_Pods_StudiousTests
+@end
