5e4dc12d1f891591ba77bcc72d6919bb55a0f001
Daniel Ankunda <dan.am@aol.com>
2020-02-26 17:20:50 -0500

    Self Profile View Button and View

--- Studious/View Controllers/SelfProfileViewController.swift
+++ Studious/View Controllers/SelfProfileViewController.swift
@@ -1,26 +1,15 @@
 //
-//  HomeViewController.swift
+//  SelfProfileViewController.swift
 //  Studious
 //
-//  Created by Daniel Ankunda on 2/12/20.
+//  Created by Daniel Ankunda on 2/26/20.
 //  Copyright © 2020 Daniel Ankunda. All rights reserved.
 //
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class SelfProfileViewController: UIViewController {
 
-    @IBOutlet weak var profileButton: UIButton!
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
-    @IBOutlet weak var nameLabel: UILabel!
-    
-    @IBOutlet weak var majorLabel: UILabel!
-    
-    @IBOutlet weak var classesLabel: UILabel!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
